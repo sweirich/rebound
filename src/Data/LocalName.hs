c41ad0bf6929f53d38b43b507e7822d1d3993c9d
Stephanie Weirich <sweirich@seas.upenn.edu>
2025-01-17 09:57:21 -0500

    new location for LocalName

---
+++ src/Data/LocalName.hs
@@ -0,0 +1,10 @@
+module Data.LocalName where
+
+newtype LocalName = LocalName {name :: String}
+  deriving (Eq, Ord)
+
+instance Show LocalName where
+  show (LocalName x) = x
+  
+internalName :: LocalName
+internalName = LocalName "_internal"
