5848d78d984004323526402bbe4e3642131aab1b
Stephanie Weirich <sweirich@seas.upenn.edu>
2025-05-30 10:22:24 -0400

    Add 'isVar' to 'Subst' to make generic impl easier.

--- src/Rebound/Env/Internal.hs
+++ src/Rebound/Env/Internal.hs
@@ -22,6 +22,7 @@ import GHC.Generics hiding (S)
 class (Subst v v) => SubstVar (v :: Nat -> Type) where
   var :: Fin n -> v n
 
+
 -- | Apply the environment throughout a term of
 -- type `c n`, replacing variables with values
 -- of type `v m`
@@ -30,13 +31,17 @@ class (SubstVar v) => Subst v c where
   default applyE :: (Generic1 c, GSubst v (Rep1 c), SubstVar v) => Env v m n -> c m -> c n
   applyE = gapplyE
   {-# INLINE applyE #-}
-
+  isVar :: c n -> Maybe (v :~: c, Fin n)
+  isVar _ = Nothing
+  {-# INLINE isVar #-}
+  
 -- Generic programming
 class GSubst v (e :: Nat -> Type) where
   gsubst :: Env v m n -> e m -> e n
 
-gapplyE :: (Generic1 c, GSubst v (Rep1 c), SubstVar v) => Env v m n -> c m -> c n
-gapplyE = applyOpt (\s x -> to1 $ gsubst s (from1 x))
+gapplyE :: forall c v m n. (Generic1 c, GSubst v (Rep1 c), Subst v c) => Env v m n -> c m -> c n
+gapplyE r e | Just (Refl, x) <- isVar @v @c e = applyEnv r x
+gapplyE r e = applyOpt (\s x -> to1 $ gsubst s (from1 x)) r e
 {-# INLINEABLE gapplyE #-}
 
 ------------------------------------------------------------------------------
