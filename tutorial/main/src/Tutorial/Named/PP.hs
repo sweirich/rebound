dd4bfa5f9e89060eae5e721609e70c099e3a9566
Stephanie Weirich <sweirich@seas.upenn.edu>
2026-03-17 11:30:48 +0100

    remove Lib

--- tutorial/main/src/Tutorial/Named/PP.hs
+++ tutorial/main/src/Tutorial/Named/PP.hs
@@ -19,8 +19,10 @@ import Prettyprinter (Doc, (<+>))
 import Prettyprinter qualified as PP
 import Control.Monad.Reader ( MonadReader(local) )
 
-import Tutorial.Lib
 import Tutorial.Named.Syntax
+
+import Text.Parsec (ParseError, 
+   SourcePos, sourceColumn, sourceLine, sourceName)
 
 
 -- | Pretty-print on a single line (using 'PP.group' to collapse line breaks)
@@ -30,6 +32,85 @@ oneline x = show (PP.group (display x initState))
 -- | Pretty-print with possible line breaks
 pp :: Display DispState a => a -> String
 pp x = show (display x initState)
+
+
+-------------------------------------------------------------------------
+-- * Display class for pretty printing with associated state, such as prececence
+
+class Display s d where
+    display :: d -> s -> Doc e
+
+-- | Display the list of elements in parentheses,
+-- punctuated by the given string.
+-- (If there is only one element, don't include the parentheses)
+multiple :: Display s a => String -> [a] -> s -> Doc e 
+multiple s [x] = display x
+multiple s xs =
+   PP.parens . PP.sep <$>
+       (PP.punctuate (PP.pretty s) <$> mapM display xs)
+
+-------------------------------------------------------------------------
+-- Display Instances for quoting, errors, source positions, names
+-------------------------------------------------------------------------
+
+instance Display s ParseError where
+  display ps di = PP.pretty (show ps)
+
+instance Display s SourcePos where
+  display p di =
+    PP.pretty (sourceName p)
+      PP.<> PP.colon
+      PP.<> PP.pretty (sourceLine p)
+      PP.<> PP.colon
+      PP.<> PP.pretty (sourceColumn p)
+      PP.<> PP.colon
+
+-------------------------------------------------------------------------
+-- Disp Instances for Prelude types
+-------------------------------------------------------------------------
+
+displayMaybe :: (t -> s -> Doc d) -> Maybe t -> s -> Doc d
+displayMaybe display m di = case m of
+  (Just a) -> PP.pretty "Just" <+> display a di
+  Nothing -> PP.pretty "Nothing"
+
+instance (Display s a) => Display s (Maybe a) where
+  display = displayMaybe display
+
+displayEither ::
+  (Display s a, Display s b) =>
+  (forall a. (Display s a) => a -> s -> Doc d) ->
+  Either a b ->
+  s ->
+  Doc d
+displayEither disp e di = case e of
+  (Left a) -> PP.pretty "Left" <+> disp a di
+  (Right a) -> PP.pretty "Right" <+> disp a di
+
+instance (Display s a, Display s b) => Display s (Either a b) where
+  display = displayEither display
+
+
+instance Display s String where
+  display = return . PP.pretty
+
+instance Display s Int where
+  display = return . PP.pretty . show
+
+instance Display s Integer where
+  display = return . PP.pretty . show
+
+instance Display s Double where
+  display = return . PP.pretty . show
+
+instance Display s Float where
+  display = return . PP.pretty . show
+
+instance Display s Char where
+  display = return . PP.pretty . show
+
+instance Display s Bool where
+  display = return . PP.pretty . show
 
 
 ------------------------------------------------
